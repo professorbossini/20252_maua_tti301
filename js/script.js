16a81608c5adc0647c7d89bb23352a2a2c750bf6
Rodrigo Bossini <professorbossini@gmail.com>
2025-08-04 09:01:17 -0300

    learn(ap01-js): encerra secao 1.2

---
+++ js/script.js
@@ -0,0 +1,33 @@
+let idade = 18
+//hoist: içamento
+console.log(`Oi, ${nome}`)
+if(idade >= 18){
+  let nome = "João"
+  console.log("Você pode dirigir, " + nome)
+}
+console.log("Até mais, " + nome)
+// var linguagem = 'Javascript'
+// console.log("Aprendendo " + linguagem)
+// linguagem = 'Java'
+// console.log('Aprendendo ' + linguagem)
+// var nome = 'José'
+// console.log(nome)
+// nome = 'José da Silva'
+// console.log(nome)
+// console.log(typeof(nome))
+
+//String nome;
+// let nome = 'José'
+// console.log(nome)
+// console.log(typeof(nome))
+// nome = 'José da Silva'
+// console.log(nome)
+// nome = 2
+// console.log(typeof(nome))
+// console.log(nome)
+// //const, let e var
+// const nome = 'José'
+// console.log(nome)
+// //não vale reatribuir
+// nome = 'José da Silva'
+// console.log(nome)
