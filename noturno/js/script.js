ccad9d678dbb1a8e75fa38ef2d7a8aea68327dac
asbonato <asbonato@gmail.com>
2025-08-13 00:00:05 -0300

    learn(ap01-js): encerra 1.4 comparação     retomar em 1.5 vetores

---
+++ noturno/js/script.js
@@ -0,0 +1,56 @@
+//comparação
+console.log(1 == 1) //true
+console.log(1 == "1") //true
+console.log(1 === 1) //true
+console.log(1 === "1") //false
+console.log(true == 1) //true
+console.log(1 == [1]) //true
+console.log(null == null) //true
+console.log(null == undefined) //true
+console.log([] == false) //true
+console.log([] == []) //false
+
+// //coerção
+// const n1 = 2
+// const n2 = '3'
+// //coerção implícita
+// console.log(n1+n2)
+// //coerção explícita
+// const n4 = n1 + Number(n2)
+// console.log(n4)
+
+// //variável pode ser redeclarada
+// var linguagem = "Javascript"
+// console.log("Aprendendo "+linguagem)
+
+// var linguagem = "Java"
+// console.log("Aprendendo "+linguagem)
+
+// var idade = 18
+// console.log(`Oi, ${nome}`)
+// //a variável é içada (do inglês hoist)
+// if (idade >= 18){
+//     var nome = "João"
+//     console.log(`Parabéns, ${nome}. Você já pode dirigir.`)
+// }
+// console.log(`Até mais, ${nome}`)
+
+
+// //declarando constantes
+// const nome = "José"
+// const idade = 27
+// //aspas simples e duplas tem o mesmo efeito
+// const sexo = "M"
+// const endereco = 'Rua K, 12'
+// const outro_end = "Rua Olho D'Água, 38"
+// //declarando variáveis
+// //let: variável tem o escopo de bloco
+// let a = 2
+// let b = "abc"
+// //var: escopo da função onde é criado ou global
+// var c = 2 + 3
+// var d = "abcd"
+
+// console.log(a)
+// console.log(b)
+// console.log(c, d)
