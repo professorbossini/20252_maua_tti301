f653237ed67ea1405f850085bc548d33b38d7151
asbonato <asbonato@gmail.com>
2025-08-19 16:29:07 -0300

    Merge branch 'main' into m-t02-sub1

--- noturno/js/script.js
+++ noturno/js/script.js
@@ -1,14 +1,94 @@
+// Discussão da comparação de objetos e referências em javascript
+
+// console.log(1 == "1")
+
+// console.log([1] == [1])
+
+// console.log([1] == "1")
+
+// var a = [1]
+// var b = [1]
+
+// console.log(a == b)
+
+// var c = a
+
+// console.log(a == c)
+
+// console.log( a === c)
+
+// console.log([1] === [1])
+
+// console.log(a == c)
+
+// console.log([1] === [1])
+
+// console.log( a === c)
+
+// Vetores
+
+var v1 = [];
+
+console.log(v1.length)
+
+v1[0] = 3.4;
+
+
+console.log(v1.length)
+
+console.log(v1[100])
+
+v1[1000] = "ab"
+
+console.log(v1.length)
+
+for (let i = 0; i < v1.length; i++) {
+    console.log(v1[i]);
+}
+
+const nomes = ["Ana Maria", "Antonio", "Rodrigo", "Alex", "Cristina"]
+
+const apenasComA = nomes.filter((n) => n.startsWith("A"))
+
+console.log("Apenas com A")
+console.log(apenasComA)
+
+const res = nomes.map((n) => n.charAt(0))
+
+console.log("Primeiras letras")
+console.log(res)
+
+const todoComecamCom = nomes.every((n) => n.startsWith("A"))
+
+console.log("Todos os nomes do vetor começam com A?")
+console.log(todoComecamCom)
+
+
+const todosFiltradosComecamComA = apenasComA.every((n) => n.startsWith("A"))
+
+console.log("A lista de nomes do vetor filtrados começando com A tem todos os itens começando com A?")
+console.log(todosFiltradosComecamComA)
+
+
+const valores = [1, 2, 3, 4];
+const soma = valores.reduce((ac, v) => ac + v);
+
+console.log(soma)
+
+
+
 //comparação
-console.log(1 == 1) //true
-console.log(1 == "1") //true
-console.log(1 === 1) //true
-console.log(1 === "1") //false
-console.log(true == 1) //true
-console.log(1 == [1]) //true
-console.log(null == null) //true
-console.log(null == undefined) //true
-console.log([] == false) //true
-console.log([] == []) //false
+// console.log(1 == 1) //true
+// console.log(1 == "1") //true
+// console.log(1 === 1) //true
+// console.log(1 === "1") //false
+// console.log(true == 1) //true
+// console.log(1 == [1]) //true
+// console.log(null == null) //true
+// console.log(null == undefined) //true
+// console.log([] == false) //true
+// console.log([] == []) //false
+
 
 // //coerção
 // const n1 = 2
