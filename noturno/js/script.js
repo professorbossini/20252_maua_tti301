8a87244a34d9f261b4c004ff3ee93cbbc56ad0e6
asbonato <asbonato@gmail.com>
2025-09-02 16:18:20 -0300

    merge with main

--- noturno/js/script.js
+++ noturno/js/script.js
@@ -1,97 +1,3 @@
-const fs = require('fs')
-const abrirArquivo = function(nomeArquivo){
-    const exibirConteudo = function(erro, conteudo){
-        if (erro) {
-            console.log(`Deu erro: ${erro}`)
-        } else {
-            console.log(conteudo.toString())
-            const dobro = conteudo.toString() * 2
-            const finalizar = function(erro){
-                if(erro){
-                    console.log(`Deu erro tentando salvar o dobro: ${erro}`)
-                } else {
-                    console.log('Salvou o dobro com sucesso')
-                }
-            }
-            fs.writeFile('dobro.txt', dobro.toString(), finalizar)
-        }
-    }
-    fs.readFile(nomeArquivo, exibirConteudo)
-}
-abrirArquivo('arquivo.txt')
-
-// const fs = require('fs')
-// const abrirArquivo = function(nomeArquivo){
-//     const exibirConteudo = function(erro, conteudo){
-//         if (erro) {
-//             console.log(`Deu erro: ${erro}`)
-//         } else {
-//             console.log(conteudo.toString())
-//         }
-//     }
-//     fs.readFile(nomeArquivo, exibirConteudo)
-// }
-// abrirArquivo('arquivo.txt')
-
-// function demorada(tempo){
-//     console.log(`demorada ${tempo}`)
-//     const atualMaisTempo = new Date().getTime() + tempo
-//     while(new Date().getTime() <= atualMaisTempo);
-//     const d = 8 + 4
-//     return d
-// }
-// setTimeout(function(){demorada(2000)}, 2000)
-// setTimeout(function(){demorada(1000)}, 1000)
-// console.log('chegou ao fim')
-
-// setTimeout(function(){
-//     console.log('dentro da timeout')
-// })
-// const a = new Date().getTime() + 1000
-// while (new Date().getTime() <= a);
-// console.log('fora da timeout')
-
-// function demorada(){
-//     const atualMais2Segundos = new Date().getTime() + 2000
-//     while(new Date().getTime() <= atualMais2Segundos);
-//     const d = 8 + 4
-//     return d
-// }
-// const a = 2 + 3
-// const b = 5 + 9
-// setTimeout(function(){
-//     const d = demorada()
-//     console.log(d)
-// }, 500)
-
-// e = 2 + a + b
-// console.log(e)
-
-// function demorada(){
-//     const atualMais2Segundos = new Date().getTime() + 2000
-//     while(new Date().getTime() <= atualMais2Segundos);
-//     const d = 8 + 4
-//     return d
-// }
-// const a = 2 + 3
-// const b = 5 + 9
-// const d = demorada()
-// e = 2 + a + b
-// console.log(e)
-
-// const a = 2 + 7
-// const b = 5
-// console.log(a + b)
-
-// console.log('Eu primeiro')
-// console.log('Agora eu')
-// console.log('Sempre vou ser a última')
-
-// let pessoa = {
-//     "nome" : "João",
-//     "idade" : 17, 
-//     nascimento: "1990-09-09"
-// }
 const axios = require("axios");
  //sua chave aqui
 const appid = "sua_chave_aqui";
@@ -366,10 +272,118 @@ axios
 //     "idade" : 17, 
 //     nascimento: "1990-09-09"
 // }
+const fs = require('fs')
+const abrirArquivo = function(nomeArquivo){
+    const exibirConteudo = function(erro, conteudo){
+        if (erro) {
+            console.log(`Deu erro: ${erro}`)
+        } else {
+            console.log(conteudo.toString())
+            const dobro = conteudo.toString() * 2
+            const finalizar = function(erro){
+                if(erro){
+                    console.log(`Deu erro tentando salvar o dobro: ${erro}`)
+                } else {
+                    console.log('Salvou o dobro com sucesso')
+                }
+            }
+            fs.writeFile('dobro.txt', dobro.toString(), finalizar)
+        }
+    }
+    fs.readFile(nomeArquivo, exibirConteudo)
+}
+abrirArquivo('arquivo.txt')
+
+// const fs = require('fs')
+// const abrirArquivo = function(nomeArquivo){
+//     const exibirConteudo = function(erro, conteudo){
+//         if (erro) {
+//             console.log(`Deu erro: ${erro}`)
+//         } else {
+//             console.log(conteudo.toString())
+//         }
+//     }
+//     fs.readFile(nomeArquivo, exibirConteudo)
+// }
+// abrirArquivo('arquivo.txt')
+
+// function demorada(tempo){
+//     console.log(`demorada ${tempo}`)
+//     const atualMaisTempo = new Date().getTime() + tempo
+//     while(new Date().getTime() <= atualMaisTempo);
+//     const d = 8 + 4
+//     return d
+// }
+// setTimeout(function(){demorada(2000)}, 2000)
+// setTimeout(function(){demorada(1000)}, 1000)
+// console.log('chegou ao fim')
+
+// setTimeout(function(){
+//     console.log('dentro da timeout')
+// })
+// const a = new Date().getTime() + 1000
+// while (new Date().getTime() <= a);
+// console.log('fora da timeout')
+
+// function demorada(){
+//     const atualMais2Segundos = new Date().getTime() + 2000
+//     while(new Date().getTime() <= atualMais2Segundos);
+//     const d = 8 + 4
+//     return d
+// }
+// const a = 2 + 3
+// const b = 5 + 9
+// setTimeout(function(){
+//     const d = demorada()
+//     console.log(d)
+// }, 500)
+
+// e = 2 + a + b
+// console.log(e)
+
+// function demorada(){
+//     const atualMais2Segundos = new Date().getTime() + 2000
+//     while(new Date().getTime() <= atualMais2Segundos);
+//     const d = 8 + 4
+//     return d
+// }
+// const a = 2 + 3
+// const b = 5 + 9
+// const d = demorada()
+// e = 2 + a + b
+// console.log(e)
+
+// const a = 2 + 7
+// const b = 5
+// console.log(a + b)
+
+// console.log('Eu primeiro')
+// console.log('Agora eu')
+// console.log('Sempre vou ser a última')
+
+// let pessoa = {
+//     "nome" : "João",
+//     "idade" : 17, 
+//     nascimento: "1990-09-09"
+// }
 
 // console.log("Me chamo " + pessoa.nome);
 // console.log("Tenho " + pessoa["idade"]);
+// console.log("Me chamo " + pessoa.nome);
+// console.log("Tenho " + pessoa["idade"]);
 
+// let pessoaComEndereco = {
+//     nome: "Marcia",
+//     idade: 21,
+//     endereco: {
+//         logradouro: "Rua B",
+//         numero:121,
+//         estado : {
+//             nome: "São Paulo",
+//             sigla: "SP"
+//         }
+//     },
+// };
 // let pessoaComEndereco = {
 //     nome: "Marcia",
 //     idade: 21,
@@ -389,7 +403,30 @@ axios
 //     trabalho como ${pessoaComEndereco.profissao}
 //     e moro na rua ${pessoaComEndereco.endereco.logradouro}, ${pessoaComEndereco.endereco.numero} - ${pessoaComEndereco.endereco.estado.nome} (${pessoaComEndereco.endereco.estado.sigla})`
 // );
+// console.log(
+//     `Sou ${pessoaComEndereco.nome},
+//     tenho ${pessoaComEndereco.idade} anos
+//     trabalho como ${pessoaComEndereco.profissao}
+//     e moro na rua ${pessoaComEndereco.endereco.logradouro}, ${pessoaComEndereco.endereco.numero} - ${pessoaComEndereco.endereco.estado.nome} (${pessoaComEndereco.endereco.estado.sigla})`
+// );
 
+// let concessionaria = {
+//     cnpj : "00011122210001-45",
+//     endereco: {
+//         logradouro: "Rua A",
+//         numero: 10,
+//         bairro: "Vila J",
+//     },
+//     veiculos: [
+//         { 
+//             marca: "Ford",
+//             modelo: "Ecosport",
+//             anoDeFabricacao: 2018,
+//         },
+//         { 
+//             marca: "Chevrolet",
+//             modelo: "ônix",
+//             anoDeFabricacao: 2020,
 // let concessionaria = {
 //     cnpj : "00011122210001-45",
 //     endereco: {
@@ -415,9 +452,18 @@ axios
 //             anoDeFabricacao: 2020,
 //         }
 //     ],
+//         },
+//         {
+//             marca: "Volkswagen",
+//             modelo: "Nivus",
+//             anoDeFabricacao: 2020,
+//         }
+//     ],
 
 // };
+// };
 
+// for (let veiculo of concessionaria.veiculos) {
 // for (let veiculo of concessionaria.veiculos) {
 //     console.log(`Marca: ${veiculo.marca}`)
 //     console.log(`Modelo: ${veiculo.modelo}`)
@@ -437,12 +483,226 @@ axios
 //         return a - b;
 //     },
 // };
+// // for (let veiculo of pessoaComEndereco.veiculos) {
+// //     console.log(`Marca: ${veiculo.marca}`)
+// //     console.log(`Modelo: ${veiculo.modelo}`)
+// //     console.log(`Ano de Fabricação: ${veiculo.anoDeFabricacao}`)
+// // }
+
+// let calculadora = {
+//     soma: 12,
+//     somar: (a,b) => a + b,
+//     subtracao: function (a, b) {
+//         return a - b;
+//     },
+// };
 
 // console.log(`2 + a = ${calculadora.somar(2, 3)}`);
 // console.log(`2 - 3 = ${calculadora.subtracao(2, 3)}`);
 // console.log("Valor da soma atual: " + calculadora.soma);
+// console.log(`2 + a = ${calculadora.somar(2, 3)}`);
+// console.log(`2 - 3 = ${calculadora.subtracao(2, 3)}`);
+// console.log("Valor da soma atual: " + calculadora.soma);
 
-let meuObjeto = { } 
+// let meuObjeto = { } 
+// let meuObjeto = { } 
+
+// //escopo interno e externo
+// function f(){
+//     let nome = 'João'
+//     function g(){
+//         console.log(nome)
+//     }
+//     g()
+// }
+// f()
+
+//closure
+// function ola(){
+//     let nome = 'João'
+//     return function (){
+//         console.log(`Olá, ${nome}`)
+//     }
+// }
+
+// olaResult = ola()
+// console.log(olaResult)
+// olaResult()
+
+// function saudacaoFactory(saudacao, nome){
+//     return function (){
+//         console.log(saudacao + ', ' + nome)
+//     }
+// }
+// let olaJoao = saudacaoFactory('Olá', 'João')
+// let tchauMaria = saudacaoFactory('Tchau', 'Maria')
+
+// olaJoao()
+// tchauMaria()
+
+// function eAgora(){
+//     let cont = 1
+//     function f1 (){
+//         console.log(cont)
+//     }
+//     cont++
+//     function f2 (){
+//         console.log(cont)
+//     }
+//     //JSON com duas funções
+//     return {f1, f2}
+
+// }
+
+// let eAgoraResult = eAgora()
+
+// eAgoraResult.f1()
+// eAgoraResult.f2()
+
+// //closure
+// //armazenando uma funçao em uma variável
+// let umaFuncao = function(){
+//     console.log("Fui armazenada em uma variável")
+// }
+// umaFuncao()
+// function triplo(n=5){
+//     console.log(3 * n)
+// }
+// //função que recebe e executa outra função
+// function f(funcao){
+//     funcao()
+// }
+// f(umaFuncao)
+// f(() => console.log(3*2))
+// f(function () {
+//     console.log('Fui criada nos parênteses de f')
+// })
+// f(triplo)
+// //criar uma função dentro e outra e retornar esta função
+// function g(){
+//     function outraFuncao(){
+//         console.log("Fui criada em g")
+//     }
+//     return outraFuncao
+// }
+// gResult = g()
+// console.log(gResult)
+// gResult()
+
+// g()()
+
+// f(g())
+
+
+
+
+// // arrow functions
+// const hello = () => console.log('Hello')
+// hello()
+// const dobro = (n) => n * 2
+// console.log(dobro(10))
+// const triplo = (n) => {
+//     return n * 3
+// }
+// console.log(triplo(4))
+
+// //funções anônimas
+// const dobro = function (n) {
+//     return n * 2
+// }
+// console.log(dobro(3))
+
+// //ter valores default para os parâmetros
+// const triplo = function (n=5){
+//     return 3*n
+// }
+// console.log(triplo(3))
+// console.log(triplo())
+
+// const quadruplo = function(n){
+//     return 2 * dobro(n)
+// }
+
+// console.log(quadruplo(4))
+
+
+
+
+
+
+// //funções
+// function hello(){
+//     console.log('Olá')
+// }
+// hello()
+
+// function hello(nome){
+//     console.log(`Olá, ${nome}`)
+// }
+// hello('João')
+
+// function soma(a, b){
+//     return a + b
+// }
+// res = soma(2, 3)
+// console.log(res)
+// console.log(soma(5, 7))
+// console.log(soma('2', '3'))
+
+// const v1 = []
+// console.log(v1)
+// v1[0] = 1
+// v1[1] = 2
+// console.log(v1)
+// v1 = []
+// Vetores
+
+// var v1 = [];
+
+// console.log(v1.length);
+
+// v1[0] = 3.4;
+
+// console.log(v1.length);
+
+// console.log(v1[100]);
+
+// v1[1000] = "ab";
+
+// console.log(v1.length);
+
+// for (let i = 0; i < v1.length; i++) {
+//   console.log(v1[i]);
+// }
+
+// const nomes = ["Ana Maria", "Antonio", "Rodrigo", "Alex", "Cristina"];
+
+// const apenasComA = nomes.filter((n) => n.startsWith("A"));
+
+// console.log("Apenas com A");
+// console.log(apenasComA);
+
+// const res = nomes.map((n) => n.charAt(0));
+
+// console.log("Primeiras letras");
+// console.log(res);
+
+// const todoComecamCom = nomes.every((n) => n.startsWith("A"));
+
+// console.log("Todos os nomes do vetor começam com A?");
+// console.log(todoComecamCom);
+
+// const todosFiltradosComecamComA = apenasComA.every((n) => n.startsWith("A"));
+
+// console.log(
+//   "A lista de nomes do vetor filtrados começando com A tem todos os itens começando com A?"
+// );
+// console.log(todosFiltradosComecamComA);
+
+// const valores = [1, 2, 3, 4];
+// const soma = valores.reduce((ac, v) => ac + v);
+
+// console.log(soma);
 
 // // Discussão da comparação de objetos e referências em javascript
 
@@ -523,6 +783,18 @@ let meuObjeto = { }
 
 
 
+// //comparação
+// // console.log(1 == 1) //true
+// // console.log(1 == "1") //true
+// // console.log(1 === 1) //true
+// // console.log(1 === "1") //false
+// // console.log(true == 1) //true
+// // console.log(1 == [1]) //true
+// // console.log(null == null) //true
+// // console.log(null == undefined) //true
+// // console.log([] == false) //true
+// // console.log([] == []) //false
+
 //comparação
 // console.log(1 == 1) //true
 // console.log(1 == "1") //true
@@ -534,7 +806,6 @@ let meuObjeto = { }
 // console.log(null == undefined) //true
 // console.log([] == false) //true
 // console.log([] == []) //false
-
 
 // // //coerção
 // // const n1 = 2
@@ -552,6 +823,15 @@ let meuObjeto = { }
 // // var linguagem = "Java"
 // // console.log("Aprendendo "+linguagem)
 
+// // var idade = 18
+// // console.log(`Oi, ${nome}`)
+// // //a variável é içada (do inglês hoist)
+// // if (idade >= 18){
+// //     var nome = "João"
+// //     console.log(`Parabéns, ${nome}. Você já pode dirigir.`)
+// // }
+// // console.log(`Até mais, ${nome}`)
+
 // var idade = 18
 // console.log(`Oi, ${nome}`)
 // //a variável é içada (do inglês hoist)
@@ -560,7 +840,6 @@ let meuObjeto = { }
 //     console.log(`Parabéns, ${nome}. Você já pode dirigir.`)
 // }
 // console.log(`Até mais, ${nome}`)
-
 
 // // //declarando constantes
 // // const nome = "José"
