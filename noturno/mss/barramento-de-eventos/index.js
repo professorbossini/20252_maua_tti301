9cf48d778bdeb7b666c9b96212ac44202c7c35b5
Marco Mazzei <marco.mazzei@maua.br>
2025-09-10 22:46:42 -0300

    Criação do barramento de eventos -até bloco 4.3.15. - Pág. 42

---
+++ noturno/mss/barramento-de-eventos/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+const axios = require('axios');
+
+app.post('/eventos', (req, res) => {
+    const evento = req.body;
+    axios.post('http://localhost:4000/eventos', evento);
+    axios.post('http://localhost:5000/eventos', evento);
+    res.status(200).send({msg: 'ok'});
+});
+
+app.listen(10000, () => {
+    console.log('Barramento de Eventos. Porta 10000');
+});
--- noturno/mss/lembretes/index.js
+++ noturno/mss/lembretes/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 
+const axios = require('axios');
+
 const lembretes = {}
 contador = 0
 
@@ -9,12 +11,25 @@ app.get('/lembretes', (req, res) => {
     res.send(lembretes)
 })
 
-app.post('/lembretes', (req, res) => {
+app.post('/lembretes', async (req, res) => {
     contador++
     const { texto } = req.body
     lembretes[contador] = { contador, texto }
+    await axios.post('http://localhost:10000/eventos', {
+        tipo: "LembreteCriado",
+        dados: {
+            contador,
+            texto,
+        }, // Faltava essa virgulo no código
+    });
     res.status(201).send(lembretes[contador])
 })
+
+app.post('/eventos', (req, res) => {
+    console.log(req.body);
+    res.status(200).send({msg: 'ok'})
+});
+
 
 app.listen(4000, () => {
     console.log('Lembretes. Porta 4000')
