7ad780f31bdc20f45397a4f63a0e585bad5c93bf
asbonato <asbonato@gmail.com>
2025-09-16 18:33:15 -0300

    preparação aula 16/09

--- noturno/mss/barramento-de-eventos/index.js
+++ noturno/mss/barramento-de-eventos/index.js
@@ -7,7 +7,7 @@ const axios = require('axios');
 app.post('/eventos', (req, res) => {
     const evento = req.body;
     axios.post('http://localhost:4000/eventos', evento);
-    axios.post('http://localhost:5000/eventos', evento);
+    axios.post('http://localhost:5001/eventos', evento);
     res.status(200).send({msg: 'ok'});
 });
 
