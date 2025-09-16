a856eb7cd1605a4d1f224a47061ae96bcaa14691
asbonato <asbonato@gmail.com>
2025-09-16 20:20:55 -0300

    learn(ap01-mss): encerra secao 4.3.40 - pg 52

--- noturno/mss/barramento-de-eventos/index.js
+++ noturno/mss/barramento-de-eventos/index.js
@@ -8,6 +8,8 @@ app.post('/eventos', (req, res) => {
     const evento = req.body;
     axios.post('http://localhost:4000/eventos', evento);
     axios.post('http://localhost:5001/eventos', evento);
+    //envia o evento para o microsserviço de consulta
+    axios.post('http://localhost:6000/eventos', evento)
     res.status(200).send({msg: 'ok'});
 });
 
