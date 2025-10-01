797f8e15fe2753988184c1d54c7044bd25f327b2
asbonato <asbonato@gmail.com>
2025-09-30 22:24:01 -0300

    learn(ap01-mss): encerra secao 4.3.49 - pg 66

--- noturno/mss/barramento-de-eventos/index.js
+++ noturno/mss/barramento-de-eventos/index.js
@@ -10,6 +10,8 @@ app.post('/eventos', (req, res) => {
     axios.post('http://localhost:5001/eventos', evento);
     //envia o evento para o microsserviço de consulta
     axios.post('http://localhost:6000/eventos', evento)
+    //envia o evento para o microsserviço de classificação
+    axios.post('http://localhost:7001/eventos', evento)
     res.status(200).send({msg: 'ok'});
 });
 
--- noturno/mss/consulta/index.js
+++ noturno/mss/consulta/index.js
@@ -13,6 +13,13 @@ const funcoes = {
             baseConsulta[observacao.lembreteId]['observacoes'] || []
         observacoes.push(observacao)
         baseConsulta[observacao.lembreteId]['observacoes'] = observacoes
+    },
+    ObservacaoAtualizada: (observacao) => {
+        const observacoes = 
+            baseConsulta[observacao.lembreteId]['observacoes']
+        const indice = 
+            observacoes.findIndex((o) => o.id === observacao.id)
+        observacoes[indice] = observacao
     }
 }
 
@@ -21,7 +28,9 @@ app.get("/lembretes", (req, res) => {
 })
 
 app.post("/eventos", (req, res) => {
-    funcoes[req.body.tipo](req.body.dados)
+    try{
+        funcoes[req.body.tipo](req.body.dados)
+    } catch(err){}
     res.status(200).send(baseConsulta)
 })
 
