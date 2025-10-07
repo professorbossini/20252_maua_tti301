4c99cbc133620fd321e04bb1ce4fae1fec56791d
Nelson Aguiar <prof.nelsonaguiar@gmail.com>
2025-10-06 22:30:12 -0300

    Memoria barramento de eventos

---
+++ noturno/Nelson_Segunda_T2-SUB2/mss - armazena eventos/observacoes/index.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+const axios = require('axios');
+
+const { v4: uuidv4 } = require('uuid');
+
+const observacoesPorLembreteId = {};
+
+const funcoes = {
+    ObservacaoClassificada: (observacao) => {
+        const observacoes =
+            observacoesPorLembreteId[observacao.lembreteId];
+            const obsParaAtualizar = observacoes.find(o => o.id === observacao.id)
+            obsParaAtualizar.status = observacao.status;
+            axios.post('http://localhost:10000/eventos', {
+            tipo: "ObservacaoAtualizada",
+            dados: {
+            id: observacao.id,
+            texto: observacao.texto,
+            lembreteId: observacao.lembreteId,
+            status: observacao.status
+            }
+        });
+    }
+}
+
+//:id é um placeholder para o id do lembrete que vai ter observações salvas sobre ele.
+app.post('/lembretes/:id/observacoes', async (req, res) => {
+    const idObs = uuidv4();
+    const { texto } = req.body;
+    const observacoesDoLembrete = observacoesPorLembreteId[req.params.id] || [];
+    observacoesDoLembrete.push({ id: idObs, texto, status: 'aguardando' });
+    observacoesPorLembreteId[req.params.id] = observacoesDoLembrete;
+    await axios.post('http://localhost:10000/eventos', {
+        tipo: "ObservacaoCriada",
+        dados: {
+            id: idObs,
+            texto,
+            lembreteId: req.params.id,
+            status: 'aguardando'
+        }
+    });
+
+    res.status(201).send(observacoesDoLembrete);
+
+});
+
+
+app.get('/lembretes/:id/observacoes', (req, res) => {
+    res.send(observacoesPorLembreteId[req.params.id] || []);
+});
+
+app.post("/eventos", (req, res) => {
+    try{
+        funcoes[req.body.tipo](req.body.dados);
+    }
+    catch (err){}
+    res.status(200).send({ msg: "ok" });
+});
+
+app.listen(5000, (()=> {
+    console.log('Observações dos Lembretes. Porta 5000');
+}))
