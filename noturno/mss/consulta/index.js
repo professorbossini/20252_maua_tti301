bf233c0f9d01065d1af82e9dc173d5821fc761f7
asbonato <asbonato@gmail.com>
2025-10-07 20:32:30 -0300

    learn(ap01-mss): encerra secao 4.3.57 - pg 81

--- noturno/mss/consulta/index.js
+++ noturno/mss/consulta/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const axios = require('axios')
 const app = express()
 app.use(express.json())
 
@@ -34,6 +35,16 @@ app.post("/eventos", (req, res) => {
     res.status(200).send(baseConsulta)
 })
 
-app.listen(6000, () => {
+app.listen(6000, async () => {
     console.log("Consultas. Porta 6000.")
+    try{
+        const resp = await axios.get('http://localhost:10000/eventos')
+        resp.data.forEach((valor, indice, colecao) => {
+            try {
+                funcoes[valor.tipo](valor.dados)
+            } catch (err) {}
+        })
+    } catch (err){
+        console.log('O barramento está fora do ar')
+    }
 })
