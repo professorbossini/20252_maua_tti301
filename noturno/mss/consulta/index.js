2bcca95ff0e3cfecf06e81d8547e21c0435448e3
asbonato <asbonato@gmail.com>
2025-10-15 11:09:32 -0300

    learn(ap01-mss): encerra secao 4.3.64 - pag 94

--- noturno/mss/consulta/index.js
+++ noturno/mss/consulta/index.js
@@ -1,50 +1,48 @@
-const express = require('express')
-const axios = require('axios')
-const app = express()
-app.use(express.json())
+const express = require("express");
+const axios = require("axios");
+const app = express();
+app.use(express.json());
 
-const baseConsulta = {}
+const baseConsulta = {};
 
 const funcoes = {
-    LembreteCriado: (lembrete) => {
-        baseConsulta[lembrete.contador] = lembrete
-    },
-    ObservacaoCriada: (observacao) => {
-        const observacoes = 
-            baseConsulta[observacao.lembreteId]['observacoes'] || []
-        observacoes.push(observacao)
-        baseConsulta[observacao.lembreteId]['observacoes'] = observacoes
-    },
-    ObservacaoAtualizada: (observacao) => {
-        const observacoes = 
-            baseConsulta[observacao.lembreteId]["observacoes"]
-        const indice = 
-            observacoes.findIndex((o) => o.id === observacao.id)
-        observacoes[indice] = observacao
-    }
-}
+  LembreteCriado: (lembrete) => {
+    baseConsulta[lembrete.contador] = lembrete;
+  },
+  ObservacaoCriada: (observacao) => {
+    const observacoes =
+      baseConsulta[observacao.lembreteId]["observacoes"] || [];
+    observacoes.push(observacao);
+    baseConsulta[observacao.lembreteId]["observacoes"] = observacoes;
+  },
+  ObservacaoAtualizada: (observacao) => {
+    const observacoes = baseConsulta[observacao.lembreteId]["observacoes"];
+    const indice = observacoes.findIndex((o) => o.id === observacao.id);
+    observacoes[indice] = observacao;
+  },
+};
 
 app.get("/lembretes", (req, res) => {
-    res.status(200).send(baseConsulta)
-})
+  res.status(200).send(baseConsulta);
+});
 
 app.post("/eventos", (req, res) => {
-    try {
-        funcoes[req.body.tipo](req.body.dados)
-    } catch (err){}
-    res.status(200).send(baseConsulta)
-})
+  try {
+    funcoes[req.body.tipo](req.body.dados);
+  } catch (err) {}
+  res.status(200).send(baseConsulta);
+});
 
 app.listen(6000, async () => {
-    console.log("Consultas. Porta 6000.")
-    try{
-        const resp = await axios.get('http://localhost:10000/eventos')
-        resp.data.forEach((valor, indice, colecao) => {
-            try {
-                funcoes[valor.tipo](valor.dados)
-            } catch (err) {}
-        })
-    } catch (err){
-        console.log('O barramento está fora do ar')
-    }
-})
+  console.log("Consultas. Porta 6000.");
+  try {
+    const resp = await axios.get("http://10.2.4.75:10000/eventos");
+    resp.data.forEach((valor, indice, colecao) => {
+      try {
+        funcoes[valor.tipo](valor.dados);
+      } catch (err) {}
+    });
+  } catch (err) {
+    console.log("O barramento está fora do ar");
+  }
+});
--- noturno/mss/observacoes/index.js
+++ noturno/mss/observacoes/index.js
@@ -11,38 +11,35 @@ const observacoesPorLembreteId = {};
 
 const funcoes = {
   ObservacaoClassificada: (observacao) => {
-    const observacoes =
-      observacoesPorLembreteId[observacao.lembreteId]
-    const obsParaAtualizar = 
-      observacoes.find(o => o.id === observacao.id)
-      obsParaAtualizar.status = observacao.status
-      axios.post('http://localhost:10000/eventos', {
-        tipo: 'ObservacaoAtualizada',
-        dados: {
-          id: observacao.id,
-          texto: observacao.texto,
-          lembreteId: observacao.lembreteId,
-          status: observacao.status,
-        }
-      })
-  }
-}
+    const observacoes = observacoesPorLembreteId[observacao.lembreteId];
+    const obsParaAtualizar = observacoes.find((o) => o.id === observacao.id);
+    obsParaAtualizar.status = observacao.status;
+    axios.post("http://10.2.4.75:10000/eventos", {
+      tipo: "ObservacaoAtualizada",
+      dados: {
+        id: observacao.id,
+        texto: observacao.texto,
+        lembreteId: observacao.lembreteId,
+        status: observacao.status,
+      },
+    });
+  },
+};
 
 //:id é um placeholder para o id do lembrete que vai ter observações salvas sobre ele.
 app.post("/lembretes/:id/observacoes", async (req, res) => {
   const idObs = uuidv4();
   const { texto } = req.body;
-  const observacoresDoLembrete = 
-    observacoesPorLembreteId[req.params.id] || [];
-  observacoresDoLembrete.push({ id: idObs, texto, status: 'aguardando' });
+  const observacoresDoLembrete = observacoesPorLembreteId[req.params.id] || [];
+  observacoresDoLembrete.push({ id: idObs, texto, status: "aguardando" });
   observacoesPorLembreteId[req.params.id] = observacoresDoLembrete;
-  await axios.post("http://localhost:10000/eventos", {
+  await axios.post("http://10.2.4.75:10000/eventos", {
     tipo: "ObservacaoCriada",
     dados: {
       id: idObs,
       texto,
       lembreteId: req.params.id,
-      status: 'aguardando'
+      status: "aguardando",
     },
   });
 
@@ -54,9 +51,9 @@ app.get("/lembretes/:id/observacoes", (req, res) => {
 });
 
 app.post("/eventos", (req, res) => {
-  try{
-    funcoes[req.body.tipo](req.body.dados)
-  } catch (err){}
+  try {
+    funcoes[req.body.tipo](req.body.dados);
+  } catch (err) {}
   res.status(200).send({ msg: "ok" });
 });
 
