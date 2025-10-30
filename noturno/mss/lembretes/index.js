95dbad43c3eae185fbb9e99bda28934cc4734fa8
Marco Mazzei <marco.mazzei@maua.br>
2025-10-29 22:50:37 -0300

    Aula de 03/10/2025 - pag. 129 - barramento de eventos service e deployment

--- noturno/mss/lembretes/index.js
+++ noturno/mss/lembretes/index.js
@@ -31,5 +31,7 @@ app.post("/eventos", (req, res) => {
 });
 
 app.listen(4000, () => {
+  console.log("Nova versão");
+  console.log("Agora usando o Docker Hub");
   console.log("Lembretes. Porta 4000");
 });
