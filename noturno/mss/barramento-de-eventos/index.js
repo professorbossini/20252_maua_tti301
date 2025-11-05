41a24824d3d67483a0a13f50bc5ec9d0e2dcebe5
asbonato <asbonato@gmail.com>
2025-11-04 21:51:30 -0300

    learn(ap01-mss): encerra a apostila

--- noturno/mss/barramento-de-eventos/index.js
+++ noturno/mss/barramento-de-eventos/index.js
@@ -9,12 +9,12 @@ const eventos = [];
 app.post("/eventos", (req, res) => {
   const evento = req.body;
   eventos.push(evento);
-  axios.post("http://10.2.4.75:4000/eventos", evento).catch((err) => {});
-  axios.post("http://10.2.4.75:5001/eventos", evento).catch((err) => {});
+  axios.post("http://lembretes-clusterip-service-sub1:4000/eventos", evento).catch((err) => {});
+  axios.post("http://observacoes-clusterip-service-sub1:5001/eventos", evento).catch((err) => {});
   //envia o evento para o microsserviço de consulta
-  axios.post("http://10.2.4.75:6000/eventos", evento).catch((err) => {});
+  axios.post("http://consulta-clusterip-service-sub1:6000/eventos", evento).catch((err) => {});
   //envia o evento para o microsserviço de classificação
-  axios.post("http://10.2.4.75:7001/eventos", evento).catch((err) => {});
+  axios.post("http://classificacao-clusterip-service-sub1:7001/eventos", evento).catch((err) => {});
   res.status(200).send({ msg: "ok" });
 });
 
